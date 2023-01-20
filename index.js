960f1cc696666faef8590aee07c4c6befee409b3
Stan Saner <stan.saner@hotmail.co.uk>
2023-01-20 21:13:13 +0000

    Added fs and inquirer packages

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
